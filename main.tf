988b79156e808d7721549592e87bd4bf3a3ee7dd
Aditya <admin@gmail.com>
2026-09-17 11:42:16 +0530

    file updated again and again

--- main.tf
+++ main.tf
@@ -333,3 +333,39 @@ resource "aws_instance" "private_ec2" {
     Name = "Private-EC2"
   }
 }
+
+# =========================================================
+# OUTPUTS
+# =========================================================
+
+output "vpc_id" {
+  value = aws_vpc.VPC01.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "nat_instance_id" {
+  value = aws_instance.nat.id
+}
+
+output "nat_private_ip" {
+  value = aws_instance.nat.private_ip
+}
+
+output "nat_public_ip" {
+  value = aws_eip.nat_eip.public_ip
+}
+
+output "private_ec2_id" {
+  value = aws_instance.private_ec2.id
+}
+
+output "private_ec2_private_ip" {
+  value = aws_instance.private_ec2.private_ip
+}
